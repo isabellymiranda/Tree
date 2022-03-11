2cb17f950532dddbc972a88e52d1f29a44ee948f
Isabelly Miranda <contatoisabellymiranda@gmail.com>
2022-03-11 01:50:31 +0000

    Envio

---
+++ Three.java
@@ -0,0 +1,10 @@
+public class Three {
+    public static void main(String[] args) { 
+System.out.println("O Múltiplos de 3 são:");
+for(int i = 0; i <= 100; i+=3){
+System.out.println(i);  
+
+} 
+}
+}
+
